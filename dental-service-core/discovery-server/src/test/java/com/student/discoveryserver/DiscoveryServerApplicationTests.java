c10e3a5ab4dffa04023a2a41199d5936b185dc10
I587570 <adam.nosol@sap.com>
2023-10-12 19:26:11 +0200

    Backend init Rename of modules

--- dental-service-core/discovery-server/src/test/java/com/student/discoveryserver/DiscoveryServerApplicationTests.java
+++ dental-service-core/discovery-server/src/test/java/com/student/discoveryserver/DiscoveryServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.student.dentalservice;
+package com.student.discoveryserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DentalServiceApplicationTests {
+class DiscoveryServerApplicationTests {
 
 	@Test
 	void contextLoads() {
